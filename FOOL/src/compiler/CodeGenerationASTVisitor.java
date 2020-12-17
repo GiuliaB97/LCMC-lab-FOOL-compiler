b2403bdaefefbf730e8e785903109d60ddd6d756
giulia <giulia.brugnatti@studio.unibo.it>
2020-12-17 11:15:05 +0100

    fix comments code generation + execute vm

--- FOOL/src/compiler/CodeGenerationASTVisitor.java
+++ FOOL/src/compiler/CodeGenerationASTVisitor.java
@@ -10,65 +10,41 @@ import static compiler.lib.FOOLlib.*;
  * l'albero è sicuramente completo perchè ho superato la fase di frontend
  * */
 
-
-/*
- * nell'ambiente globale c'è solo l'ar della funzione
- * visual vm vicino all'istruzione ci sono i numeri di linea non gli indirizzi 
- * stack : a sx
- * heap al centro
- * file fool.asm : a dx
- * la prima dichiarazione + sempre a offset -2 1000=memmsize, 9998
- * prima cosa da fare settare il control link lfp: frame pointer del chiamante
- * avanzo nella creazione dell'ar 
- * aggiungo la dichiarazione dei parametri in ordine inverso(push 5 e 3)ù
- * trovo all'access link, dovrei risalire la catena ma qui è locale quindi dico direttamente che punta all'ar dell'ambiente globale
- * duplico la dichiarazione (mi serve per settare l'access link(9994), e per trovare l'address dove è dichiarata la funzione(9998) (stm ltm ltm)  
- * con lw carico 20, con js salto e metto nel registro ra l'istruzione alla quale devo tornare; 
- * settp fp che deve ora pntare all'ar creato 9994 carico il return address sullo stakc, poi avrei le dichairazionji locali, poi il corpo della funzione, va prendere i due parametri
- * parte da 994 e a offset 1 trova il primo parametro, a offset 2 il secondo 
- */
-public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidException> {
-
-  CodeGenerationASTVisitor() {}
-  CodeGenerationASTVisitor(boolean debug) {super(false,debug);} //enables print for debugging
 /*
  * prima di eseguire il corpo del programma deve allocare l'activation record 
  * (seguendo il layout nel file di esempio(devono rispettare gli offset scelti)) perchè
  * il corpo del programma poi userà le variabili che altrimenti non potrebbero essere raggiungibili
+ * DICHIARAZIONI
+ * "dec": varDec: dichiarazioni di variabili; 
+ * Le dichiarazioni: partono da offset -2 [stanno stack]	
  *
  * nlJoin:
  * alla prima dichairazione il risultato è visita di dec perchè null viene ignorato dalla funzione
  * poi concatena il risutlato della visita successiva,
  * quindi alloca le variabili una ad una"
  */
+public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidException> {
+
+  CodeGenerationASTVisitor() {}
+  CodeGenerationASTVisitor(boolean debug) {super(false,debug);} //enables print for debugging
+
 	@Override
 	public String visitNode(ProgLetInNode n) {
 		if (print) printNode(n);
-		String declCode = null;/*qui metto il codice generato considerando le dichiarazioni 
-								nel loro ordine
-		 						per le dichiarazioni: partendo da offset -1
-								 *lo genero un po' per volta visitando le dichairazioni
-								 *le "dec" sono var dec: dichiarazioni di variabili
-								 */
-		//l'istruzione seguente genera il codice di allocazione per ogni variabile
-		for (Node dec : n.declist) declCode = nlJoin(declCode, visit(dec));//aggiungo a declcode il risultaoto della funzione
+		String declCode = null;/*qui metto il codice generato considerando le dichiarazioni nel loro ordine.		*/
+		
+		for (Node dec : n.declist) declCode = nlJoin(declCode, visit(dec));//l'istruzione seguente genera il codice di allocazione per ogni variabile,aggiunge a declcode il risultato della visita alla dichiarazione
 		
 		return nlJoin(
-				"push 0", /*devo impostare un return address fittizio a 0 o 
-							quello che mi pare nessuno lo legegerà ma devo 
-							uniformare l'ambiente globale a quello delle funzioni
-							*/
-				declCode,// generate code for declarations (allocation)
-							//lei mi deve mettere nello stack il suo indirizzo
-				visit(n.exp),
-				"halt", 
-				getCode());
+				"push 0", /*devo impostare un return address fittizio a 0 non verrà letto perchè il main va in halt(=terminazione programma), ma mi serve per uniformare l'ambiente globale a quello delle funzioni se no gli offset ecc. mi si scazzano tutti*/
+				declCode,// generate code for declarations (allocation); mette sullo stack il valore con cui le dichiarazioni sono inizializzate
+				visit(n.exp),//visito il corpo del programma
+				"halt",// halt(=terminazione programma)
+				getCode());//recupera dalla classe che mette a disposizione le funzioni di libreria il codice creato per l'intero programma; fool lib c'è come un campo apposito
 	}
 /*
- * programma con corpo senza dichiarazioni di variabili ecc
- * devo solo ritornare il risultato
- * occhio che qui devo uscire dalla vm
- * alla fine del main c'è halt
+ * programma con corpo SENZA dichiarazioni di variabili ecc.
+ * AIM: devo solo ritornare il risultato
  */
 	@Override
 	public String visitNode(ProgNode n) {
@@ -79,52 +55,59 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		);
 	}
 /*
- * allocare la dichairazione di una funzione 
- * vuol dire metterci il suo indirizzo 
+ * Qui stiamo terminando l'AR che ha iniziato a costruire la call
+ * 
+ * fare in modo che la posizione di riferimento punti all'access link; essendo che call lo ha appena messo; la prima chosa che faccio è settare fp in modo che punti; così sono a posto
+ * il chiamante ha usato js per saltare qui, js salta all'inidirizzo che c'pè sulla cima della stack e memorizza in il return address ossia l'inirizzo dell'istruzione successiva al js; devo memorizzarlo sullo stack; in quanto parte dell'AR , è l'indirizzo al quale dovrò tornare
+ * necessario allorale dichiarazioni locali della funzione, ognuna alloca una cosa sola, in casso di variabile il risultato della loro espressione di inizializzazione, o l'indirizzo della funzione dichiarata in caso di funzioni
+ * 
+ * NB i parametri della callNode sono stati messi in ordine inverso per allocati con offset da 1 a N, mentre le dichiarazioni hanno offset da -2 a -N sono nello stack
+ * NB perché è fondamentale settare fp all'inizio? Dentro declCode posso avere dichiarazioni di variabili che sono inizializzati con espressioni che devono essere valutate in quale ambiente? Potrebbe usare per nella sua espresssione variaili che abbiamo appena dichiarato, che quindi deve cercare nell'activation record in cui siamo, quindi fp deve essere già settato.
+ * 
+ * visito il corpo della funzione
+ * salvo in temp il risultato della funzione 
+ * inizio a smontare l'ar
+ * rimuovo le local declaration
+ * rimuovo il return addrees, mettendolo in ra così ce l'ho per quando devo saltare indietro al chaimante
+ * rimuovo l'access link(ultima cosa allocata dal chiamante)
+ * rimuovo gli argomenti (allocati dal chiamante prima di chiamarci)
+ * rimuovo il control link, mi serviva per ripristinare il frame pointer alla posizione di riferimento del fram del chiamante perchè questi possa continuare l'esecuzione come se nulla fosse successo
+ * recupero il risultato della funzione carico il return address da ra
+ * e salto indietro al chiamante
+ * 
+ * REMEMBER
+ * allocare la dichairazione di:
+ * - una funzione vuol dire metterci il suo indirizzo 
+ * - una variabile vuole dire metterci il valore con cui è inizializzata
  */
 	@Override
 	public String visitNode(FunNode n) {
 		if (print) printNode(n,n.id);
 		String declCode = null, popDecl = null, popParl = null;
 		for (Node dec : n.declist) {
-			declCode = nlJoin(declCode,visit(dec));
-			popDecl = nlJoin(popDecl,"pop");
+			declCode = nlJoin(declCode,visit(dec)); //inserisce il valore con cui è inizializzata ogni dichiarazione
+			popDecl = nlJoin(popDecl,"pop");	//inserisce un pop per ogni dichiarazione presente, perchè: dopo andranno tutte rimosse dallo stack, la funzione deve lasciare lo stack inalterato, aggiungendovi solo il suo risultato.
 		}
-		for (int i=0;i<n.parlist.size();i++) popParl = nlJoin(popParl,"pop");
-		String funl = freshFunLabel();
+		for (int i=0;i<n.parlist.size();i++) popParl = nlJoin(popParl,"pop"); //insierisce un pop per ogni argomento presente NB gli argomenti sono già stati messi sullo stack da call
+		String funl = freshFunLabel();//viene allocata tramite push 
 		//questo è il momento giusto perchè ascolta i primi 10 minuti
 		putCode(
-			nlJoin(
-					//devo finire di allocare l'activation record
+			nlJoin( //devo finire di allocare l'activation record creato da call [chiamante
 				funl+":",
 				"cfp", // set $fp to $sp value
 				"lra", // load $ra value //il chiamante ha usato js per arrivare a noi, nel js c'è l'istruzione successiva a js alla quale dobbiamo tornare
-				//le dichiarazioni delle funzioni allocano ciascuna qualcosa, se è una dichiarazione di var le allochiamo con il valore calcolato dalla loro espressione, se sono funzioni le allochiamo con la push dell'indirizzo
-				declCode, // generate code for local declarations (they use the new $fp!!!)
-				//parametri li metto in ordine inverso perchè primo parametro ha offset 1, secondo 2 (?) ma le dichiarazioni le mettiamo nello stesso ordine in cui compaiono del codice
-				//è fondamentantale settare fp all'inizio perchè dentro declcode, posso avere funzioni con paraemtri e roba locali a un certo punto può fare dei calcoli che usano var appena dichiarate e dove le trova? le trov anell'activation record in cui siamo, lui la x la raggiunge tramite il registro fp, il registro fp deve essere settato in modo che punti all'activation record in cui siamo (?),
-				
-				
+				declCode, // generate code for local declarations (they use the new $fp!!!)		
 				visit(n.exp), // generate code for function body expression
-				
-				//salvo il valore del corpo della funzione nel registro tmp
 				"stm", // set $tm to popped value (function result)
 				
 				//inizio a rimuovere l'activation record
-				//rimuovo le dichiarazioni
 				popDecl, // remove local declarations from stack
-				//rimuovo il return address: lo prendo e lo metto in ra così quando devo saltaare lo vado a prendere e per saltare indietro al chiamante
 				"sra", // set $ra to popped value
-				//rimuovo l'access link, allocato dal chiamante come ultima cosa 
 				"pop", // remove Access Link from stack
-				//il chiamante aveva allocato gli argomenti che ci aveva passatojk
 				popParl, // remove parameters from stack
-				//rimuovo il control link con fp, il control link mi serve per ripristinare il frame pointer alla posizione dle chiamante(Come se la chiamata a funzione non fosse avevanuta
 				"sfp", // set $fp to popped value (Control Link)
-				//recupero il valore della funzione (unica cosa che deve restare sullo stack
 				"ltm", // load $tm value (function result)
 				"lra", // load $ra value
-				//salto al chiamante
 				"js"  // jump to to popped address
 			)
 		);
@@ -177,9 +160,7 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		return nlJoin(
 				visit(n.cond), 
 				"push 1", 	//pusho e controllo se è uguale al risultato della visita	
-				"beq "+l1,	//se sono uguali salto allo then 
-							//if(cond)then{1}else{0}; 
-							//(0 == false e 1== true)
+				"beq "+l1,	//se sono uguali salto allo then //if(cond)then{1}else{0}; //(0 == false e 1== true)
 				visit(n.el),
 				"b "+l2,	//Se ho fatto l'else salto alla fine 
 				l1+":",
@@ -240,20 +221,16 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		return nlJoin(
 			visit(n.left), 
 			visit(n.right),
-			"add"/*codice che prende i valori sullo stack e 
-				fa il risultato della somma*/
-		);/*ci metto l'nljoin perchè ogni volta che ho più di 
-			un argomento lo devo usare*/
+			"add"/*codice che prende i valori sullo stack e fa il risultato della somma*/
+		);/*ci metto l'nljoin perchè ogni volta che ho più di un argomento lo devo usare*/
 	}
-
+	
 	@Override
 	public String visitNode(CallNode n) {
 		if (print) printNode(n,n.id);
 		String argCode = null, getAR = null;
 		for (int i=n.arglist.size()-1;i>=0;i--) argCode=
-				nlJoin(argCode,visit(n.arglist.get(i)));
-		//n.nl =nesting level a cui è fatta la chiamata
-		//n.entry.nl= nesting level a cui è dichiarata la funzione 
+				nlJoin(argCode,visit(n.arglist.get(i)));//parametri li metto in ordine inverso perchè primo parametro ha offset 1, secondo 2 (?) ma le dichiarazioni le mettiamo nello stesso ordine in cui compaiono del codice 
 		for (int i = 0;i<n.nl-n.entry.nl;i++) getAR=nlJoin(getAR,"lw");
 		return nlJoin(
 				"lfp", // load Control Link (pointer to frame of function "id" caller)
@@ -269,32 +246,25 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	            "js"  // jump to popped address (saving address of subsequent instruction in $ra)
 			);//tutto per capire dove devo saltare
 	}
-
+	/*	getAr:
+	 *  -    per raggiungere il frame corretto si deve risalire la catena degli AL dopo aver fatto lw 
+			punto al frame che mi richiude sintatticamente ed in particolare punto al suo AL.
+	 *  -   codice che mi serve per raggiunger l'ar della dichiarazione; 
+	 *  	iterando sulla differenza di nesting level ad ogni iterazione aggiungo una lw alla stringa.
+	 *  		n.nl =nesting level a cui è fatta la chiamata
+	 *  		n.entry.nl= nesting level a cui è dichiarata la funzione
+	 */
 	@Override
 	public String visitNode(IdNode n) {
 		if (print) printNode(n,n.id);	
 		String getAR = null;
-		for (int i = 0; i < n.nl - n.entry.nl; i++) getAR = nlJoin(getAR, "lw");
-		/*	getAr codice che mi serve per raggiunger l'ar della dichiarazione; 
-			iterando sulla differenza di nesting level ad ogni iterazione 
-			aggiungo una lw alla stringa
-		*/
-	
+		for (int i = 0; i < n.nl - n.entry.nl; i++) getAR = nlJoin(getAR, "lw");	
 		return nlJoin(
-				"lfp",						//retrieve address of frame containing "id" declaration
-				getAR, 					/*per raggiungere il frame corretto devo risalire la catena 
-											degli access link dopo aver fatto lw punto al frame che mi 
-											richiude sintatticamente ed in particolare punto al suo 
-											access link
-											*/
-											// by following the static chain (of Access Links)
-				"push" +n.entry.offset,	//metto l'offset sullo stack
-											//compute address of "id" declaration
-				"add",					//metto sullo stack l'indiriizzo della var
-				"lw" 					/*prende l'indirizzo sulla cima dello stack, 
-										lo poppa e mette al suo posto i valore della variabile corrisposndente
-										*/
-											//load value of "id" variable
+				"lfp",	//retrieve address of frame containing "id" declaration
+				getAR, // by following the static chain (of Access Links)
+				"push" +n.entry.offset,	"add",//compute address of "id" declaration (addr=indirizzo AL + offset)
+				"lw" 					/*prende l'indirizzo sulla cima dello stack, lo poppa e mette al suo posto i valore della variabile corrispondente*/
+										//load value of "id" variable
 				);
 	}
 
@@ -303,11 +273,11 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		if (print) printNode(n,n.val.toString());
 		return "push "+(n.val?1:0);
 	}
-
+	
+	//remeber: da rispettare l'invariante lo stack va lasciato come lo si è trovato con il regalo per la mamma in cima
 	@Override
 	public String visitNode(IntNode n) {
 		if (print) printNode(n,n.val.toString());
 		return "push "+n.val; //la conversione viene fatta in un automatico (questo è quello che passo alla famosa funzione del punto 4)
-							//remeber: da rispettare l'invariante lo stack va lasciato come lo si è trovato con il regalo per la mamma in cima
 	}
 }
